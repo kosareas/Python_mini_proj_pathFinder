658b7f81b1d5c73fd8d4db0d1a5a2260c8e35539
Bhushan <brn2393@gmail.com>
2022-03-29 22:03:52 +0530

    Pathfinder

---
+++ myprg.py
@@ -0,0 +1,6 @@
+print('hello')
+a=int(input('enter nnumbber'))
+if a%2==0:
+ print('even')
+else:
+ print('odd')
---
+++ s.py
@@ -0,0 +1,4 @@
+x=(input('enter values'))
+print(x)
+tuplesd=tuple('enter tupls')
+print(tuplesd)
